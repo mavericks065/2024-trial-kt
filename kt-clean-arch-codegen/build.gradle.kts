5fd0ec541297bc15a6350a3f92e7eaa705cbdd68
manolisan <manastasiou@kpler.com>
2024-06-21 18:54:05 +0300

    feat: try codegen validation

--- kt-clean-arch-codegen/build.gradle.kts
+++ kt-clean-arch-codegen/build.gradle.kts
@@ -1,52 +1,39 @@
 import org.hidetake.gradle.swagger.generator.GenerateSwaggerCode
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.net.URL
+import java.nio.file.Files
+import java.nio.file.StandardCopyOption
 
 plugins {
-	kotlin("plugin.jpa") version "1.9.24"
-	kotlin("jvm") version "1.9.24"
-	kotlin("plugin.spring") version "1.9.24"
-	id("org.springframework.boot") version "3.3.1-SNAPSHOT"
-	id("io.spring.dependency-management") version "1.1.5"
-	id ("org.hidetake.swagger.generator") version "2.19.2"
+	kotlin("jvm") version "1.8.0"
+	kotlin("plugin.spring") version "1.8.0"
+	kotlin("plugin.jpa") version "1.8.0"
+	id("org.springframework.boot") version "3.0.0"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("org.hidetake.swagger.generator") version "2.18.2"
 }
 
-group = "com.nig"
+
+group = "com.example"
 version = "0.0.1-SNAPSHOT"
-
-java {
-	sourceCompatibility = JavaVersion.VERSION_21
-}
+java.sourceCompatibility = JavaVersion.VERSION_21
+val openApiGeneratorVersion = "6.1.0"
+val openApiGeneratorCliJar = "openapi-generator-cli-$openApiGeneratorVersion.jar"
+val openApiGeneratorCliUrl = "https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/$openApiGeneratorVersion/$openApiGeneratorCliJar"
+val openApiGeneratorCliPath = "$buildDir/libs/openapi-generator-cli.jar"
 
 repositories {
 	mavenCentral()
-	maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
+	implementation("io.swagger.parser.v3:swagger-parser-v3:2.0.31")
+	swaggerCodegen("org.openapitools:openapi-generator-cli:7.6.0")
 
-	//open api code generation
-	swaggerCodegen("org.openapitools:openapi-generator-cli:7.3.0")
-	implementation ("io.swagger.core.v3:swagger-annotations:2.2.20")
-
-	//open api ui
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
-
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-		jvmTarget = JvmTarget.JVM_21
-	}
 }
 
 tasks.withType<Test> {
@@ -59,6 +46,40 @@ tasks {
 	}
 }
 
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+	kotlinOptions {
+		jvmTarget = "21" // Set the JVM target to a supported version
+	}
+}
+
+tasks.register("downloadOpenApiGeneratorCli") {
+	val outputFile = file(openApiGeneratorCliPath)
+	outputs.file(outputFile)
+
+	doLast {
+		if (!outputFile.exists()) {
+			println("Downloading $openApiGeneratorCliJar")
+			val url = URL(openApiGeneratorCliUrl)
+			url.openStream().use { input ->
+				Files.copy(input, outputFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
+			}
+		} else {
+			println("$openApiGeneratorCliJar already exists, skipping download.")
+		}
+	}
+}
+
+
+tasks.register<Exec>("validateOpenApi") {
+	dependsOn("downloadOpenApiGeneratorCli")
+	group = "verification"
+	description = "Validates the OpenAPI specification file"
+
+	val openApiFilePath = "${rootDir}/src/main/resources/api/stocks.yaml"
+
+	commandLine("java", "-jar", "$buildDir/libs/openapi-generator-cli.jar", "validate", "-i", openApiFilePath)
+}
+
 swaggerSources {
 	create("stocks") .apply {
 		setInputFile(file("${rootDir}/src/main/resources/api/stocks.yaml"))
@@ -67,7 +88,7 @@ swaggerSources {
 			outputDir = file("codegen")
 			configFile = file("${rootDir}/src/main/resources/api/config.json")
 			components = mapOf("models" to true, "apis" to true, "supportingFiles" to "ApiUtil.kt")
-			dependsOn("validation") //first validate the yaml
+			dependsOn("validateOpenApi") //first validate the yaml
 		})
 	}
 }
@@ -81,12 +102,3 @@ sourceSets {
 		resources.srcDir("${swaggerSources["stocks"].code.outputDir}/src/main/resources")
 	}
 }
-//val stocks by swaggerSources.creating {
-//	inputFile.set(file("${rootDir}/src/main/resources/api/stocks.yaml"))
-//	code {
-//		language.set("spring")
-//		configFile.set(file("${rootDir}/src/main/resources/api/config.json"))
-//		components.set(mapOf("models" to true, "apis" to true, "supportingFiles" to "ApiUtil.kt"))
-//		dependsOn("validation") //first validate the yaml
-//	}
-//}
